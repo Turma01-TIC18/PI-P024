178c7922656ea375ef2784cd2e12399a860f70b9
FranklinMenezes2309 <fmpereira2309@hotmail.com>
2023-10-20 16:51:49 -0300

    criação da estrutura pi-p024

---
+++ exercicio-02.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Evento {
+    //implementar
+    
+};
+
+class Roteiro : public Evento {
+    //implementar
+    
+};
+
+class Deslocamento : public Evento {
+   //implementar
+        
+};
+
+class Pernoite : public Evento {
+   //implementar
+        
+};
+
+class Dependente {
+    //implementar
+
+};
+
+class Pacote {
+    //implementar
+    
+};
+
+class Cliente;
+
+class Reserva {
+
+   //implementar
+        
+};
+
+class Cliente {
+    //implementar
+
+};
+
+class AgenciaViagens {
+//implementar
+
+};
+
+int main() {
+    //implementar
+
+    
+   
+
+    return 0;
+}
