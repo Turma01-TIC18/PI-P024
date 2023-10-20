2e92eedcfce0d71866054947526badc07ee52fe6
FranklinMenezes2309 <fmpereira2309@hotmail.com>
2023-10-20 20:44:56 -0300

    implementação da classe dependente e pacote

--- exercicio-02.cpp
+++ exercicio-02.cpp
@@ -5,32 +5,76 @@
 using namespace std;
 
 class Evento {
-    //implementar
+    public:
+        string nome;
+        string descricao;
+
+        Evento(string _nome, string _descricao) {
+            nome = _nome;
+            descricao = _descricao;
+        }
     
 };
 
 class Roteiro : public Evento {
-    //implementar
+    public:
+        vector<string> destinos;
+        
+        Roteiro(string _nome, string _descricao, vector<string> _destinos)
+            : Evento(_nome, _descricao) {
+                destinos = _destinos;
+        }
     
 };
 
 class Deslocamento : public Evento {
-   //implementar
+    public:
+        string meioDeTransporte;
+
+        Deslocamento(string _nome, string _descricao, string _meioDeTransporte)
+            : Evento(_nome, _descricao) {
+                meioDeTransporte = _meioDeTransporte;
+            }
         
 };
 
 class Pernoite : public Evento {
-   //implementar
+    public:
+        string hotel;
+        string dataCheckin;
+        string dataCheckout;
+
+        Pernoite(string _nome, string _descricao, string _hotel, string _dataCheckin, string _dataCheckout)
+            : Evento(_nome, _descricao) {
+                hotel = _hotel;
+                dataCheckin = _dataCheckin;
+                dataCheckout = _dataCheckout;
+            }
         
 };
 
 class Dependente {
-    //implementar
+    public:
+        string nome;
+        int idade;
+
+        Dependente(string _nome, int _idade) {
+            nome = _nome;
+            idade = _idade;
+        }
 
 };
 
 class Pacote {
-    //implementar
+    public:
+        string nome;
+        string descricao;
+        vector<Evento*> eventos; 
+
+        Pacote(string _nome, string _descricao) {
+            nome = _nome;
+            descricao = _descricao;
+        }
     
 };
 
